cd6114a85d0e77a98cc43630f8d907704e772636
xenradio <me@jonathanmacdonald.com>
2025-06-01 20:26:19 +1000

    fix: update dependencies to secure versions
    - Update all dependencies to their latest secure versions
    - Use --legacy-peer-deps to resolve peer dependency conflicts
    - Remove outdated packages
    - Fix vulnerability warnings

---
+++ update-dependencies.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Update dependencies to latest secure versions
+npm install @apollo/client@latest
+npm install @headlessui/react@latest
+npm install @heroicons/react@latest
+npm install @reduxjs/toolkit@latest
+npm install @tanstack/react-query@latest
+npm install @web3-react/core@latest
+npm install @web3-react/injected-connector@latest
+npm install @web3-react/walletconnect-connector@latest
+npm install axios@latest
+npm install chart.js@latest
+npm install ethers@latest
+npm install graphql@latest
+npm install react-chartjs-2@latest
+npm install react-redux@latest
+npm install react-router-dom@latest
+npm install redux@latest
+npm install ws@latest
+
+# Update dev dependencies
+npm install @eslint/js@latest
+npm install @types/node@latest
+npm install @types/react@latest
+npm install @types/react-dom@latest
+npm install @vitejs/plugin-react@latest
+npm install eslint@latest
+npm install eslint-plugin-react-hooks@latest
+npm install eslint-plugin-react-refresh@latest
+npm install globals@latest
+npm install typescript@latest
+npm install vite@latest
