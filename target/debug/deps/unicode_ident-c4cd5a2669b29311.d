f1aa37054e7724a334acb6b7b5b308882aa41a14
SELF-HQ <me@jonathanmacdonald.com>
2025-07-04 14:37:54 +1000

    Start fresh with minimal GitHub Pages setup

--- target/debug/deps/unicode_ident-c4cd5a2669b29311.d
+++
@@ -1,8 +0,0 @@
-/Users/jmac/Documents/GitHub/self-chain-public/target/debug/deps/libunicode_ident-c4cd5a2669b29311.rmeta: /Users/jmac/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs /Users/jmac/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs
-
-/Users/jmac/Documents/GitHub/self-chain-public/target/debug/deps/libunicode_ident-c4cd5a2669b29311.rlib: /Users/jmac/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs /Users/jmac/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs
-
-/Users/jmac/Documents/GitHub/self-chain-public/target/debug/deps/unicode_ident-c4cd5a2669b29311.d: /Users/jmac/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs /Users/jmac/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs
-
-/Users/jmac/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs:
-/Users/jmac/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs:
